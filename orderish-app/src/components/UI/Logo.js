1ccf562d11d7decfd4d54cd17c6f96f6b87d261b
marben <mailbenhar@gmail.com>
2021-10-08 23:23:11 -0500

    added appbar

---
+++ orderish-app/src/components/UI/Logo.js
@@ -0,0 +1,6 @@
+import logo from "./OrderishLogo.png";
+import classes from "./Logo.module.css";
+const Logo = (props) => {
+  return <img className={classes.logo} src={logo} alt="Logo"></img>;
+};
+export default Logo;
